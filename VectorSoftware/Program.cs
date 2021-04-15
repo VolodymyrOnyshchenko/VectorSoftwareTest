3a8c9d589f2cb9952068f10cba3c1ea2e4b4f441
Vova Andreichuk <vandreichuk2000@gmail.com>
2021-04-15 15:39:16 +0300

    vector test

---
+++ VectorSoftware/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace VectorSoftwareTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "WEAREDISCOVEREDFLEEATONCE";
+            int key= 3;
+            Encrypt(input, key);
+        }
+        private static string Encrypt (string input, int key)
+        {
+            string output = "";
+            int size = input.Length;
+            char[,] array = new char [key,size];
+
+            for (int r = 0, c = 0, d = 1; c < size; r += d, ++c)
+            {
+                array[r,c] = input[c];
+                if (r == key - 1)
+                    d = -1;
+                else if (r == 0)
+                    d = 1;
+            }
+            for (int r = 0; r < key; ++r)
+            {
+                for (int c = 0; c < size; ++c)
+                {
+                    if (array[r, c] != 0)
+                        output += array[r, c];
+                }
+            }
+            Console.WriteLine(output);
+            return output;
+        }
+
+
+        private static string Decrypt (string input, int key)
+        {
+            string output = "";
+            int size = input.Length;
+            char[,] array = new char[key, size];
+            for (int r = 0, c = 0, d = 1; c < size; r += d, ++c)
+            {
+                array[r, c] = '*';
+                if (r == key - 1)
+                    d = -1;
+                else if (r == 0)
+                    d = 1;
+            }
+
+            int count = 0;
+            for(int r = 0; r < key; ++r)
+            {
+                for(int c = 0; c < size; ++ c)
+                {
+                    if (array[r, c] == '*')
+                        array[r, c] = input[count++];
+                }
+            }
+            for(int r = 0, c = 0, d = 1;c < size; r += d, ++c)
+            {
+                output += array[r, c];
+                if (r == key - 1)
+                    d = -1;
+                else if (r == 0)
+                    d = 1;
+            }
+            Console.WriteLine(output);
+            return output;
+        }
+
+    }
+}
+
+
+         
+        
+
+
+
+
---
+++ VectorSoftwareTask2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Shape : IComparable<Shape>
+{
+    public double Area { get; set; }
+
+    public int CompareTo(Shape any)
+    {
+        return Area.CompareTo(any.Area);
+    }
+
+}
+
+public class Square : Shape
+{
+    public Square(double side)
+    {
+        Area = side * side;
+    }
+}
+
+public class Rectangle : Shape
+{
+    public Rectangle(double width, double height)
+    {
+        Area = width * height;
+    }
+}
+
+public class Triangle : Shape
+{
+    public Triangle(double Base, double height)
+    {
+        Area = (Base * height) / 2;
+    }
+}
+
+public class Circle : Shape
+{
+    public Circle(double radius)
+    {
+        Area = (radius * radius) * Math.PI;
+    }
+}
