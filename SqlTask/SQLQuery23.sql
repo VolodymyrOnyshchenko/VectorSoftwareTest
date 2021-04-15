6ac33ec9b4d118964d914fa5071dcbc5231fd301
Vova Andreichuk <vandreichuk2000@gmail.com>
2021-04-15 15:40:58 +0300

    test

--- SqlTask/SQLQuery23.sql
+++ SqlTask/SQLQuery23.sql
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT * FROM ProductsTable
 JOIN SuppliersTable ON SuppliersTable.SupplierID = ProductsTable.SupplierID
 JOIN CategoriesTable ON CategoriesTable.CategoryID = ProductsTable.CategoryID
 
 
---Select product with product name that begins with ‘C’.
+--Select product with product name that begins with ï¿½Cï¿½.
 --WHERE ProductName LIKE 'C%'
 
 --Select product with the smallest price.
@@ -28,34 +27,3 @@ INSERT INTO ProductsTable (ProductName, Price, SupplierID,CategoryID)
 VALUES ('Green tea', '10', '6', '1')
 */
 
-=======
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT * FROM ProductsTable
-JOIN SuppliersTable ON SuppliersTable.SupplierID = ProductsTable.SupplierID
-JOIN CategoriesTable ON CategoriesTable.CategoryID = ProductsTable.CategoryID
-
-
---Select product with product name that begins with ‘C’.
---WHERE ProductName LIKE 'C%'
-
---Select product with the smallest price.
---WHERE Price = (SELECT MIN(Price) FROM ProductsTable)
-
---Select cost of all products from the USA.
---WHERE Country = 'USA'
-
---Select suppliers that supply Condiments.
---WHERE CategoryName = 'Condiments'
-/*
-INSERT INTO SuppliersTable (SupplierName,City,Country)
-VALUES ('Norske Meierier', 'Lviv', 'Ukraine')
-
-
-INSERT INTO CategoriesTable (CategoryName, Description)
-VALUES ('Beverages', 'Soft drinks, coffees, teas, beers, and ales')
-
-INSERT INTO ProductsTable (ProductName, Price, SupplierID,CategoryID)
-VALUES ('Green tea', '10', '6', '1')
-*/
-
->>>>>>> 8264fe20aaac38ed64aec12fc6893e36551aa224
